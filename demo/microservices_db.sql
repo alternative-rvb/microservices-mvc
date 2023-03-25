2f32ff94ae26810734b99fc759a8835af1762368
Nicolas <alternative.rvb@gmail.com>
2023-03-25 19:16:30 +0100

    Refactoring

--- demo/microservices_db.sql
+++ demo/microservices_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mer. 22 mars 2023 à 22:55
+-- Généré le : sam. 25 mars 2023 à 18:15
 -- Version du serveur : 8.0.30
 -- Version de PHP : 8.1.10
 
@@ -41,13 +41,13 @@ CREATE TABLE `ms_posts` (
 --
 
 INSERT INTO `ms_posts` (`microservice_id`, `Titre`, `Contenu`, `Prix`, `Image`, `user_id`) VALUES
-(1, 'Création de site vitrine', 'Développer un site web simple et élégant pour présenter une entreprise, un produit ou un service, avec des pages de présentation, une galerie d&#039;images et des coordonnées.', 5.00, '641b86d600356.jpg', 1),
-(2, 'Intégration d&#039;un blog', 'Ajouter un blog à un site web existant, en utilisant une plateforme de gestion de contenu (CMS) comme WordPress ou Ghost, et en personnalisant le design pour qu&#039;il corresponde à l&#039;identité visuelle du site.', 7.00, '641b86c8b1410.jpg', 2),
-(3, 'Mise en place d&#039;une boutique en ligne', 'Intégrer une solution e-commerce comme WooCommerce, Shopify ou PrestaShop à un site web existant, pour permettre la vente de produits ou services en ligne.', 5.00, '641b86a3ea846.jpg', 3),
-(4, 'Optimisation du référencement naturel (SEO)', 'Améliorer le classement d&#039;un site web dans les moteurs de recherche en optimisant la structure du site, les balises méta, le contenu et en créant des liens de qualité.', 2.00, '641b8698a7c78.jpg', 4),
-(5, 'Création d&#039;un formulaire de contact', 'Concevoir et intégrer un formulaire de contact personnalisé sur un site web, avec validation des données saisies et envoi des informations par e-mail au propriétaire du site.', 5.00, '641b8692549aa.jpg', 5),
-(6, 'Adaptation de thèmes et plugins', 'Personnaliser un thème ou un plugin existant pour répondre aux besoins spécifiques d&#039;un client, en modifiant le code source ou en ajoutant des fonctionnalités supplémentaires.', 5.00, '641b868aa85ed.jpg', 6),
-(7, 'Maintenance et mises à jour', 'Assurer la maintenance d&#039;un site web, y compris les mises à jour du système, des plugins et du contenu, la résolution des problèmes de compatibilité et la mise en place de mesures de sécurité.', 8.00, '641b8601312c0.jpg', 7),
+(1, 'Création de site vitrine', 'Développer un site web simple et élégant pour présenter une entreprise, un produit ou un service, avec des pages de présentation, une galerie d&#039;images et des coordonnées.', 5.00, '641f39d61310b.jpg', 1),
+(2, 'Intégration d&#039;un blog', 'Ajouter un blog à un site web existant, en utilisant une plateforme de gestion de contenu (CMS) comme WordPress ou Ghost, et en personnalisant le design pour qu&#039;il corresponde à l&#039;identité visuelle du site.', 7.00, '641f39ce78a71.jpg', 1),
+(3, 'Mise en place d&#039;une boutique en ligne', 'Intégrer une solution e-commerce comme WooCommerce, Shopify ou PrestaShop à un site web existant, pour permettre la vente de produits ou services en ligne.', 5.00, '641f39c2a9c1b.jpg', 1),
+(4, 'Optimisation du référencement naturel (SEO)', 'Améliorer le classement d&#039;un site web dans les moteurs de recherche en optimisant la structure du site, les balises méta, le contenu et en créant des liens de qualité.', 2.00, '641f39b23d947.jpg', 1),
+(5, 'Création d&#039;un formulaire de contact', 'Concevoir et intégrer un formulaire de contact personnalisé sur un site web, avec validation des données saisies et envoi des informations par e-mail au propriétaire du site.', 5.00, '641f398fa1276.jpg', 1),
+(6, 'Adaptation de thèmes et plugins', 'Personnaliser un thème ou un plugin existant pour répondre aux besoins spécifiques d&#039;un client, en modifiant le code source ou en ajoutant des fonctionnalités supplémentaires.', 5.00, '641f397a76204.jpg', 1),
+(7, 'Maintenance et mises à jour', 'Assurer la maintenance d&#039;un site web, y compris les mises à jour du système, des plugins et du contenu, la résolution des problèmes de compatibilité et la mise en place de mesures de sécurité.', 8.00, '641f396e71059.jpg', 1),
 (65, '', '', 0.00, '', 0);
 
 -- --------------------------------------------------------
@@ -70,7 +70,7 @@ CREATE TABLE `ms_users` (
 --
 
 INSERT INTO `ms_users` (`user_id`, `Nom`, `Prénom`, `Email`, `Password`, `Rôle`) VALUES
-(1, 'Doe', 'John', 'john.doe@example.com', '$2y$10$BWhNQ00D6eKuA5AhV0KrOu1TqMK.uZrPuapy8HMiX3s8JYMF9dpyC', 1),
+(1, 'Doe', 'John', 'john.doe@example.com', '$2y$10$BWhNQ00D6eKuA5AhV0KrOu1TqMK.uZrPuapy8HMiX3s8JYMF9dpyC', 0),
 (2, 'Stracke', 'Francesco', '', NULL, NULL),
 (3, 'MacGyver', 'Melyna', '', NULL, NULL),
 (4, 'Mayert', 'Ivory', '', NULL, NULL),
